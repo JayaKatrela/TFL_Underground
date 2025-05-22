91e6008d7bf45887ab5475b7587c59def5587347
JayaKatrela <nagalakshmi.k95@gmail.com>
2025-05-22 21:51:46 +0100

    Update cdcspark.py

--- cdcspark.py
+++ cdcspark.py
@@ -1,11 +1,8 @@
 from pyspark.sql import SparkSession
 # Create a SparkSession
 spark = SparkSession.builder \
-    .appName("CDC Application") \
-    .config("spark.hadoop.fs.s3a.aws.credentials.provider", "com.amazonaws.auth.profile.ProfileCredentialsProvider") \
-    .config("spark.hadoop.fs.s3a.credentials.file", "C:/Users/nagal/.aws/credentials") \
+    .appName("nameof application ") \
     .config("spark.hadoop.fs.s3a.impl", "org.apache.hadoop.fs.s3a.S3AFileSystem") \
-    .config("spark.hadoop.fs.s3a.path.style.access", "true") \
     .config("spark.jars.packages", "org.apache.hadoop:hadoop-aws:3.2.0") \
     .getOrCreate()
 
