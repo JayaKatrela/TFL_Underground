f6bc1384f95d10d2c183fb9f8472669ae2e5b6da
JayaKatrela <nagalakshmi.k95@gmail.com>
2025-05-27 14:02:55 +0100

    Update cdcspark.py

--- cdcspark.py
+++ cdcspark.py
@@ -28,7 +28,7 @@ for idx, row in enumerate(metadata_rows, start=1):
     last_synced_at = row['last_synced_at']
 
     # Step 3: Query for incremental records only
-      if last_synced_at:
+if last_synced_at:
     query = "(SELECT * FROM {} WHERE {} > '{}') AS temp".format(table, tracking_col, last_synced_at)
 else:
     query = "(SELECT * FROM {}) AS temp".format(table)
